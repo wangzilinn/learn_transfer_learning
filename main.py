6958ad8d525afdc46261a64a21b15fa663139328
wangzilin <wangzilinn@gmail.com>
2021-06-01 19:29:43 +0800

    终于实现了迁移学习 可以识别猫猫了

---
+++ main.py
@@ -0,0 +1,116 @@
+import tensorflow as tf
+# 获取数据
+import tensorflow_datasets as tfds
+
+tfds.disable_progress_bar()
+
+train_ds, validation_ds, test_ds = tfds.load(
+    "cats_vs_dogs",
+    # Reserve 10% for validation and 10% for test
+    split=["train[:40%]", "train[40%:50%]", "train[50%:60%]"],
+    as_supervised=True,  # Include labels
+)
+
+print("Number of training samples: %d" % tf.data.experimental.cardinality(train_ds))
+print(
+    "Number of validation samples: %d" % tf.data.experimental.cardinality(validation_ds)
+)
+print("Number of test samples: %d" % tf.data.experimental.cardinality(test_ds))
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 10))
+for i, (image, label) in enumerate(train_ds.take(9)):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(image)
+    plt.title(int(label))
+    plt.axis("off")
+plt.show()
+
+# 标准化数据
+
+size = (150, 150)
+
+train_ds = train_ds.map(lambda x, y: (tf.image.resize(x, size), y))
+validation_ds = validation_ds.map(lambda x, y: (tf.image.resize(x, size), y))
+test_ds = test_ds.map(lambda x, y: (tf.image.resize(x, size), y))
+
+batch_size = 32
+
+train_ds = train_ds.cache().batch(batch_size).prefetch(buffer_size=10)
+validation_ds = validation_ds.cache().batch(batch_size).prefetch(buffer_size=10)
+test_ds = test_ds.cache().batch(batch_size).prefetch(buffer_size=10)
+
+# 使用随机数据扩充
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+data_augmentation = keras.Sequential(
+    [
+        layers.experimental.preprocessing.RandomFlip("horizontal"),
+        layers.experimental.preprocessing.RandomRotation(0.1),
+    ]
+)
+
+import numpy as np
+
+for images, labels in train_ds.take(1):
+    plt.figure(figsize=(10, 10))
+    first_image = images[0]
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        augmented_image = data_augmentation(
+            tf.expand_dims(first_image, 0), training=True
+        )
+        plt.imshow(augmented_image[0].numpy().astype("int32"))
+        plt.title(int(labels[0]))
+        plt.axis("off")
+plt.show()
+
+# 构建模型
+
+base_model = keras.applications.Xception(
+    weights="imagenet",  # Load weights pre-trained on ImageNet.
+    input_shape=(150, 150, 3),
+    include_top=False,
+)  # Do not include the ImageNet classifier at the top.
+
+# Freeze the base_model
+base_model.trainable = False
+
+# Create new model on top
+inputs = keras.Input(shape=(150, 150, 3))
+x = data_augmentation(inputs)  # Apply random data augmentation
+
+# Pre-trained Xception weights requires that input be normalized
+# from (0, 255) to a range (-1., +1.), the normalization layer
+# does the following, outputs = (inputs - mean) / sqrt(var)
+norm_layer = keras.layers.experimental.preprocessing.Normalization()
+mean = np.array([127.5] * 3)
+var = mean ** 2
+# Scale inputs to [-1, +1]
+x = norm_layer(x)
+norm_layer.set_weights([mean, var])
+
+# The base model contains batchnorm layers. We want to keep them in inference mode
+# when we unfreeze the base model for fine-tuning, so we make sure that the
+# base_model is running in inference mode here.
+x = base_model(x, training=False)
+x = keras.layers.GlobalAveragePooling2D()(x)
+x = keras.layers.Dropout(0.2)(x)  # Regularize with dropout
+outputs = keras.layers.Dense(1)(x)
+model = keras.Model(inputs, outputs)
+
+model.summary()
+
+# 训练顶层
+
+model.compile(
+    optimizer=keras.optimizers.Adam(),
+    loss=keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=[keras.metrics.BinaryAccuracy()],
+)
+
+epochs = 20
+model.fit(train_ds, epochs=epochs, validation_data=validation_ds)
